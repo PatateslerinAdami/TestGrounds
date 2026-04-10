ffba7253bfabc3f93c4cf7624b05d3af6ea23ee6
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-04-10 17:25:39 +0200

    Aatrox R update

--- Content/LeagueSandbox-Scripts/Characters/Aatrox/R.cs
+++ Content/LeagueSandbox-Scripts/Characters/Aatrox/R.cs
@@ -1,68 +1,71 @@
-﻿using GameServerCore.Enums;
+﻿using System.Linq;
+using System.Numerics;
+using GameServerCore.Enums;
 using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.Buildings;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.Scripting.CSharp;
 using static LeagueSandbox.GameServer.API.ApiFunctionManager;
-namespace Spells
-{
-    public class AatroxR : ISpellScript
-    {
-        public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
-        {
-            TriggersSpellCasts = true
-        };
 
-        string pcastname;
-        string phitname;
+namespace Spells;
 
-        public void OnActivate(ObjAIBase owner, Spell spell)
-        {
-            if (owner is Champion c)
-            {
-                if (c.SkinID == 0)
-                {
-                    pcastname = "Aatrox_Base_R_Activate";
-                    phitname = "Aatrox_Base_R_active_hit_tar";
-                }
-                else if (c.SkinID == 1)
-                {
-                    pcastname = "Aatrox_Skin01_R_Activate";
-                    phitname = "Aatrox_Skin01_R_active_hit_tar";
-                }
-                else if (c.SkinID == 2)
-                {
-                    pcastname = "Aatrox_Skin02_R_Activate";
-                    phitname = "Aatrox_Skin02_R_active_hit_tar";
-                }
-            }
-        }
+public class AatroxR : ISpellScript {
+    private ObjAIBase _aatrox;
+    private string    _pcastname;
+    private string    _phitname;
 
-        public void OnSpellCast(Spell spell)
-        {
-            var owner = spell.CastInfo.Owner;
-            AddParticleTarget(owner, owner, pcastname, owner);
-        }
+    public SpellScriptMetadata ScriptMetadata { get; } = new() {
+        TriggersSpellCasts = false
+        // TODO
+    };
 
-        public void OnSpellPostCast(Spell spell)
-        {
-            if (spell.CastInfo.Owner is Champion c)
-            {
-                var damage = 200 + (100 * (spell.CastInfo.SpellLevel - 1)) + (c.Stats.AbilityPower.Total);
-                var units = GetUnitsInRange(c.Position, 550f, true);
-
-                for (int i = 0; i < units.Count; i++)
-                {
-                    if (units[i].Team != c.Team && !(units[i] is ObjBuilding || units[i] is BaseTurret))
-                    {
-                        units[i].TakeDamage(c, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-                        AddParticleTarget(c, units[i], phitname, units[i]);
-                    }
-                }
-
-                AddBuff("AatroxR", 12f, 1, spell, spell.CastInfo.Owner, spell.CastInfo.Owner);
-            }
+    public void OnActivate(ObjAIBase owner, Spell spell) {
+        _aatrox = owner;
+        switch (_aatrox.SkinID) {
+            case 0:
+                _pcastname = "Aatrox_Base_R_Activate";
+                _phitname  = "Aatrox_Base_R_active_hit_tar";
+                break;
+            case 1:
+                _pcastname = "Aatrox_Skin01_R_Activate";
+                _phitname  = "Aatrox_Skin01_R_active_hit_tar";
+                break;
+            case 2:
+                _pcastname = "Aatrox_Skin02_R_Activate";
+                _phitname  = "Aatrox_Skin02_R_active_hit_tar";
+                break;
         }
     }
+
+    public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end) {
+
+        AddParticleTarget(_aatrox, _aatrox, _pcastname, _aatrox);
+
+        float apRatio = _aatrox.Stats.AbilityPower.Total;
+        float damage  = 200 + (100 * spell.CastInfo.SpellLevel - 1) + apRatio;
+        //AddParticle(_aatrox, _aatrox, "Aatrox_Base_R_Aura_Self", _aatrox.Position);
+        
+
+        var enemiesInRange = GetUnitsInRange(_aatrox, _aatrox.Position, 550, true,
+                                             SpellDataFlags.AffectEnemies | SpellDataFlags.AffectHeroes |
+                                             SpellDataFlags.AffectMinions | SpellDataFlags.AffectNeutral);
+        foreach (var enemy in enemiesInRange) {
+            AddParticleTarget(_aatrox, enemy, _phitname, enemy);
+            enemy.TakeDamage(_aatrox, damage, DamageType.DAMAGE_TYPE_MAGICAL, DamageSource.DAMAGE_SOURCE_SPELLAOE,
+                            false);
+            SpellCast(_aatrox, 5, SpellSlotType.ExtraSlots, true, _aatrox, enemy.Position);
+        }
+        AddBuff("AatroxR", 12f, 1, spell, spell.CastInfo.Owner, spell.CastInfo.Owner);
+    }
+}
+
+public class AatroxRHeal : ISpellScript {
+    public SpellScriptMetadata ScriptMetadata { get; } = new() {
+        TriggersSpellCasts = true,
+        MissileParameters = new MissileParameters() {
+          Type  = MissileType.Target,
+        },
+    };
 }
