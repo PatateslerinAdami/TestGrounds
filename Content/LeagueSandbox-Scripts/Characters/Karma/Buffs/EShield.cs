cede69a4bb01d7d72e9447d3058f2692a4d2e0b5
aurora-selena <266846874+aurora-selena@users.noreply.github.com>
2026-04-23 08:36:29 +0200

    Karma shield fix

--- Content/LeagueSandbox-Scripts/Characters/Karma/Buffs/EShield.cs
+++ Content/LeagueSandbox-Scripts/Characters/Karma/Buffs/EShield.cs
@@ -5,6 +5,7 @@ using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
 using GameServerCore.Enums;
+using GameServerLib.GameObjects.AttackableUnits;
 using LeagueSandbox.GameServer.GameObjects.StatsNS;
 using LeagueSandbox.GameServer.GameObjects;
 
@@ -13,9 +14,11 @@ namespace Buffs
 {
     internal class KarmaSolKimShield : IBuffGameScript
     {
+        private ObjAIBase _karma;
+        private Shield _solKimShield;
         public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
-            BuffType = BuffType.INVISIBILITY,
+            BuffType = BuffType.COMBAT_ENCHANCER,
             BuffAddType = BuffAddType.REPLACE_EXISTING,
             MaxStacks = 1
         };
@@ -23,12 +26,13 @@ namespace Buffs
 
         public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
-            unit.TakeShield(200f, 200f);
+            _karma = ownerSpell.CastInfo.Owner;
+            _solKimShield = new Shield(_karma, _karma, true, true, 200f);
         }
 
         public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
-
+            RemoveShield(_karma, _solKimShield);
         }
     }
 }
