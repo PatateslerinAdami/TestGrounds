2df0f5935156a038cdae530ee226413dc9077be5
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-04-23 07:08:06 +0200

    Shield and fade .cs

---
+++ GameServerLib/GameObjects/Fade.cs
@@ -0,0 +1,18 @@
+namespace GameServerLib.GameObjects
+{
+    public class Fade
+    {
+        internal readonly int Id;
+        internal readonly float StartTime;
+        internal readonly float Duration;
+        internal readonly float Opacity;
+
+        internal Fade(int id, float startTime, float duration, float opacity)
+        {
+            Id = id;
+            StartTime = startTime;
+            Duration = duration;
+            Opacity = opacity;
+        }
+    }
+}
