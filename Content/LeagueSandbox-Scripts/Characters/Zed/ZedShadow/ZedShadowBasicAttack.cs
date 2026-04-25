d39781066e58fe2e8e434d3938fd2e69dd0be01d
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-04-25 23:46:37 +0200

    Zed update and Minion scripts init

---
+++ Content/LeagueSandbox-Scripts/Characters/Zed/ZedShadow/ZedShadowBasicAttack.cs
@@ -0,0 +1,22 @@
+using System.Numerics;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+
+namespace Spells;
+
+public class ZedShadowBasicAttack : ISpellScript {
+
+    public SpellScriptMetadata ScriptMetadata => new() {
+
+        IsDamagingSpell = false
+    };
+}
---
+++ Content/LeagueSandbox-Scripts/Characters/Zed/ZedShadow/ZedUltFakeMissile.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Sector;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace Spells;
+
+public class ZedUltFakeMissile : ISpellScript {
+
+    public SpellScriptMetadata ScriptMetadata => new() {
+    };
+}
