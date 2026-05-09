f984259156be97990bb361ff34ba5d7385da1f61
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-05-09 12:31:59 +0200

    char script name fix

--- Content/LeagueSandbox-Scripts/Monsters/AncientGolem/WrathoftheAncients.cs
+++ Content/LeagueSandbox-Scripts/Monsters/AncientGolem/WrathoftheAncients.cs
@@ -5,7 +5,7 @@ using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
 namespace CharScripts;
 
-internal class CharScriptAncientGolem : ICharScript {
+internal class WrathoftheAncients : ICharScript {
     public void OnActivate(ObjAIBase owner, Spell spell = null) {
         AddBuff("GlobalMonsterBuff",      25000.0f, 1, spell, owner, owner, true);
         AddBuff("CrestoftheAncientGolem", 25000.0f, 1, spell, owner, owner, true);
