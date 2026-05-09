30380ed3f59175168f5f959ad0f73e7d8b41bc3b
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-05-09 13:18:39 +0200

    Comment to explain difference

--- GameServerLib/Game.cs
+++ GameServerLib/Game.cs
@@ -38,7 +38,7 @@ namespace LeagueSandbox.GameServer
         // Function Vars
         private static ILog _logger = LoggerProvider.GetLogger();
         private float _nextSyncTime = 10 * 1000;
-        protected const double REFRESH_RATE = 1000.0 / 30.0; // GameLoop called 60 times a second.
+        protected const double REFRESH_RATE = 1000.0 / 30.0; // GameLoop called either 30 times (normal mode) or 60 times (tournament mode) a second.
         private HandleStartGame _gameStartHandler;
 
         // Server
