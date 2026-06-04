76c850652f485bc5131cb0a978792e6751989e33
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-06-04 18:32:12 +0200

    verified buffscriptmetadata fields added

--- GameServerLib/Scripting/CSharp/BuffScriptMetaData.cs
+++ GameServerLib/Scripting/CSharp/BuffScriptMetaData.cs
@@ -9,5 +9,20 @@ namespace LeagueSandbox.GameServer.Scripting.CSharp
         public int MaxStacks { get; set; } = 1;
         public bool IsHidden { get; set; } = false;
         public bool UpdateInfinite { get; set; } = false;
+        
+        public bool IsNonDispellable { get; set; } = false; //this is important for things like cleanse or self cc's that should not be removed
+        
+        public bool PersistsThroughDeath { get; set; } = true;
+        
+        public bool PermeatesThroughDeath { get; set; } = false;
+
+        public bool IsDeathRecapSource { get; set; } = false;
+
+        public int OnPreDamagePriority { get; set; } = 0;
+
+        public bool DoOnPreDamageInExpirationOrder { get; set; } = false;
+
+        
+
     }
 }
