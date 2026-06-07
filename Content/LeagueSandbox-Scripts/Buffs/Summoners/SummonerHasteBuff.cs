4ed6b1bd6c6585f6921f58534874237f3b33bb6c
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-06-07 22:54:37 +0200

    fixed ghost buff

--- Content/LeagueSandbox-Scripts/Buffs/Summoners/SummonerHasteBuff.cs
+++ Content/LeagueSandbox-Scripts/Buffs/Summoners/SummonerHasteBuff.cs
@@ -1,15 +1,19 @@
-﻿using GameServerCore.Enums;
+﻿using System.Numerics;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
 using LeagueSandbox.GameServer.GameObjects;
 using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
 using LeagueSandbox.GameServer.GameObjects.SpellNS;
 using LeagueSandbox.GameServer.GameObjects.StatsNS;
-using GameServerCore.Scripting.CSharp;
 using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
 
 namespace Buffs
 {
     internal class SummonerHasteBuff : IBuffGameScript
     {
+        private Particle _p1, _p2;
         public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
         {
             BuffType = BuffType.HASTE,
@@ -21,8 +25,17 @@ namespace Buffs
 
         public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
         {
+            var owner = ownerSpell.CastInfo.Owner;
             StatsModifier.MoveSpeed.PercentBonus = 27 / 100.0f;
             unit.AddStatModifier(StatsModifier);
+            _p1 = AddParticleTarget(owner, unit, "Global_SS_Ghost", unit);
+            _p2 = AddParticleTarget(owner, unit, "Global_SS_Ghost_cas", unit);
+        }
+
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            RemoveParticle(_p1);
+            RemoveParticle(_p2);
         }
     }
 }
