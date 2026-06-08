fde19c499c475aa7cbad7d8807e5b987128d5a3f
aurora-selena <266846874+aurora-selena@users.noreply.github.com>
2026-06-08 09:56:23 +0200

    vladimnir init

---
+++ Content/LeagueSandbox-Scripts/Characters/Vladimir/Buffs/VladimirSanguinePool.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using GameServerCore.Enums;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+
+
+namespace Buffs
+{
+    internal class VladimirSanguinePool : IBuffGameScript {
+        private ObjAIBase _vladimir;
+        private PeriodicTicker _periodicTicker;
+        private const float MaxTickAmount = 4;
+        private const float MaxMovespeedAmount = 37.5f;
+        private int _tickAmount = 4;
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType    = BuffType.COMBAT_ENCHANCER,
+            BuffAddType = BuffAddType.REPLACE_EXISTING,
+            MaxStacks   = 1
+        };
+        public StatsModifier StatsModifier { get; private set; } = new StatsModifier();
+
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            _vladimir = ownerSpell.CastInfo.Owner;
+            unit.SetStatus(StatusFlags.Targetable, false);
+            HideHealthBar(unit, -1, true);
+            StatsModifier.MoveSpeed.PercentBonus += MaxMovespeedAmount;
+            _vladimir.AddStatModifier(StatsModifier);
+        }
+
+        public void OnUpdate(float diff)
+        {
+            var tick = _periodicTicker.ConsumeTicks(diff, 1000f, false, 1, (int)MaxTickAmount);
+            if (tick != 1) return;
+            _tickAmount--;
+            _vladimir.RemoveStatModifier(StatsModifier);
+            StatsModifier.MoveSpeed.PercentBonus = MaxMovespeedAmount/MaxTickAmount * _tickAmount;
+            _vladimir.AddStatModifier(StatsModifier);
+        }
+
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            unit.SetStatus(StatusFlags.Targetable, true);
+            HideHealthBar(unit, -1, false);
+        }
+    }
+}
---
+++ Content/LeagueSandbox-Scripts/Characters/Vladimir/W.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.SpellNS.Missile;
+using LeagueSandbox.GameServer.Logging;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace Spells;
+
+public class VladimirSanguinePool : ISpellScript
+{
+    private ObjAIBase _vladimir;
+
+    public SpellScriptMetadata ScriptMetadata => new() {
+        TriggersSpellCasts = true,
+        IsDamagingSpell = true,
+    };
+
+    public void OnActivate(ObjAIBase owner, Spell spell)
+    {
+        _vladimir = owner;
+    }
+
+    public void OnSpellCast(Spell spell)
+    {
+        AddBuff("VladimirSanguinePool", 2f, 1, spell, _vladimir, _vladimir);
+    }
+}
