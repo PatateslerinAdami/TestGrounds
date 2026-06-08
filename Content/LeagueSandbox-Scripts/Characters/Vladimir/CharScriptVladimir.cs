8272adc0ab999f838b311680e93015d9bfb3df9e
aurora-selena <266846874+aurora-selena@users.noreply.github.com>
2026-06-08 09:56:57 +0200

    vlad passive

---
+++ Content/LeagueSandbox-Scripts/Characters/Vladimir/CharScriptVladimir.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+
+namespace CharScripts;
+
+public class CharScriptVladimir : ICharScript {
+    public void OnActivate(ObjAIBase owner, Spell spell) {
+
+    }
+}
