c7374bd768ec2224d343afc3ddaaaf5d94e508d7
aurora-selena <266846874+aurora-selena@users.noreply.github.com>
2026-07-02 11:39:32 +0200

    Implemented Master Yi DoubleStrike passive

---
+++ Content/LeagueSandbox-Scripts/Characters/Master Yi/Buffs/DoubleStrike.cs
@@ -0,0 +1,48 @@
+﻿using GameServerCore.Enums;
+using GameServerCore.Scripting.CSharp;
+using LeagueSandbox.GameServer.GameObjects;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.GameObjects.SpellNS;
+using LeagueSandbox.GameServer.GameObjects.StatsNS;
+using LeagueSandbox.GameServer.Scripting.CSharp;
+using System.Numerics;
+using static LeagueSandbox.GameServer.API.ApiFunctionManager;
+using GameMaths;
+using GameServerLib.GameObjects.AttackableUnits;
+using LeagueSandbox.GameServer.API;
+using LeagueSandbox.GameServer.GameObjects.AttackableUnits.AI;
+
+namespace Buffs
+{
+    internal class DoubleStrike : IBuffGameScript
+    {
+
+        private ObjAIBase _masterYi;
+        public BuffScriptMetaData BuffMetaData { get; set; } = new BuffScriptMetaData
+        {
+            BuffType = BuffType.DAMAGE,
+            BuffAddType = BuffAddType.RENEW_EXISTING,
+            MaxStacks = 1,
+            IsNonDispellable = true
+        };
+
+        public StatsModifier StatsModifier { get; } = new();
+        public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            _masterYi = ownerSpell.CastInfo.Owner;
+        }
+
+        public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
+        {
+            DamageType damageType = buff.Variables.Get("damageType", DamageType.DAMAGE_TYPE_PHYSICAL);
+            DamageResultType damageResultType = buff.Variables.Get("damageResultType", DamageResultType.RESULT_NORMAL);
+            AttackableUnit target = buff.Variables.Get<AttackableUnit>("target");
+            if (!target.IsDead)
+            {
+                target.TakeDamage(_masterYi, buff.Variables.GetFloat("damage", 0f) * 0.5f, damageType,
+                    DamageSource.DAMAGE_SOURCE_ATTACK, damageResultType);
+            }
+            _masterYi.ResetAutoAttackSpell();
+        }
+    }
+}
