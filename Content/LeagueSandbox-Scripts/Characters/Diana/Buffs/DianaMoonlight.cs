e97a6b74d570c594ba92ea8df485b5c8ed5a5779
Aurora-Selena <266846874+aurora-selena@users.noreply.github.com>
2026-07-02 22:09:34 +0200

    Diana Moonlight fixes

--- Content/LeagueSandbox-Scripts/Characters/Diana/Buffs/DianaMoonlight.cs
+++ Content/LeagueSandbox-Scripts/Characters/Diana/Buffs/DianaMoonlight.cs
@@ -14,6 +14,7 @@ namespace Buffs;
 public class DianaMoonlight : IBuffGameScript {
     private ObjAIBase _diana;
     private Region _bubbleRegion;
+    private Particle _moonlightParticle;
     public BuffScriptMetaData BuffMetaData { get; set; } = new() {
         BuffType = BuffType.COMBAT_DEHANCER,
         BuffAddType = BuffAddType.REPLACE_EXISTING
@@ -24,12 +25,13 @@ public class DianaMoonlight : IBuffGameScript {
     public void OnActivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
     {
         _diana = ownerSpell.CastInfo.Owner;
-        AddParticleTarget(_diana, unit, "Diana_Base_Q_Moonlight.troy", unit, buff.Duration);
+        _moonlightParticle = AddParticleTarget(_diana, unit, "Diana_Base_Q_Moonlight.troy", unit, buff.Duration);
         _bubbleRegion = AddUnitPerceptionBubble(unit, 480f, buff.Duration, _diana.Team, revealSpecificUnitOnly: unit);
     }
 
     public void OnDeactivate(AttackableUnit unit, Buff buff, Spell ownerSpell)
     {
+        RemoveParticle(_moonlightParticle);
         _bubbleRegion.SetToRemove();
     }
 }
