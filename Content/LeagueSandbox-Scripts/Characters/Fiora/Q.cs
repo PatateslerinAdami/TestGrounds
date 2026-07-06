e4e5b97e5afbb0c370e69b311b09ae90dcebc16f
aurora-selena <266846874+aurora-selena@users.noreply.github.com>
2026-07-06 15:42:00 +0200

    moving fiora q state over to git

--- Content/LeagueSandbox-Scripts/Characters/Fiora/Q.cs
+++ Content/LeagueSandbox-Scripts/Characters/Fiora/Q.cs
@@ -17,87 +17,93 @@ namespace Spells
 {
     public class FioraQ : ISpellScript
     {
-        private ObjAIBase Fiora;
-        private Spell FioraDash;
-        private AttackableUnit Target;
+        private ObjAIBase _fiora;
+        private Spell _spell;
+        private AttackableUnit _target;
         public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
+            NotSingleTargetSpell = false,
+            DoesntBreakShields = true,
             TriggersSpellCasts = true,
+            CastingBreaksStealth = true,
             IsDamagingSpell = true
         };
-        
+
+        public void OnActivate(ObjAIBase owner, Spell spell)
+        {
+            _fiora = owner;
+        }
+
         public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
-            Target = target;
-            FioraDash = spell;
-            Fiora = owner = spell.CastInfo.Owner as Champion;
+            _target = target;
+            _spell = spell;
         }
         
         public void OnSpellPostCast(Spell spell)
         {
-            Fiora.CancelAutoAttack(true);
-            if (!Fiora.HasBuff("FioraQCD")) { AddBuff("FioraQCD", 4, 1, spell, Fiora, Fiora); }
-            SpellCast(Fiora, 0, SpellSlotType.ExtraSlots, false, Target, Vector2.Zero);
+            _fiora.CancelAutoAttack(true);
+            if (!_fiora.HasBuff("FioraQCD")) { AddBuff("FioraQCD", 4, 1, spell, _fiora, _fiora); }
+            SpellCast(_fiora, 0, SpellSlotType.ExtraSlots, false, _target, Vector2.Zero);
         }
     }
 
     public class FioraQLunge : ISpellScript
     {
-        float Dist;
-        float Damage;
-        Vector2 TargetPos;
-        private ObjAIBase Fiora;
-        private Spell FioraDash;
-        private AttackableUnit Target;
+        private float _damage;
+        private ObjAIBase _fiora;
+        private Spell _spell;
+        private AttackableUnit _target;
         public SpellScriptMetadata ScriptMetadata { get; private set; } = new SpellScriptMetadata()
         {
             IsDamagingSpell = true,
             TriggersSpellCasts = true
         };
-        
+
+        public void OnActivate(ObjAIBase owner, Spell spell)
+        {
+            _fiora = owner;
+        }
+
         public void OnSpellPreCast(ObjAIBase owner, Spell spell, AttackableUnit target, Vector2 start, Vector2 end)
         {
-            Target = target;
-            FioraDash = spell;
-            Fiora = owner = spell.CastInfo.Owner as Champion;
-            Fiora.SetTargetUnit(null, true);
-            SetStatus(Fiora, StatusFlags.Ghosted, true);
-            Fiora.CancelAutoAttack(true);
+            _target = target;
+            _spell = spell;
+            _fiora.SetTargetUnit(null, true);
+            SetStatus(_fiora, StatusFlags.Ghosted, true);
+            _fiora.CancelAutoAttack(true);
         }
         
         public void OnSpellCast(Spell spell)
         {
-            ApiEventManager.OnMoveEnd.AddListener(this, Fiora, OnMoveEnd, true);
-            ApiEventManager.OnMoveSuccess.AddListener(this, Fiora, OnMoveSuccess, true);
-            Dist = System.Math.Abs(Vector2.Distance(Target.Position, Fiora.Position)) - 125;
-            TargetPos = GetPointFromUnit(Fiora, Dist);
-            PlayAnimation(Fiora, "Spell1");
-            FaceDirection(TargetPos, Fiora, true);
-            AddParticleTarget(Fiora, Fiora, "Fiora_Dance_windup.troy", Fiora);
-            AddParticleTarget(Fiora, Fiora, "FioraQLunge_dashtrail.troy", Fiora);
-            ForceMove(Fiora, TargetPos, 2200, orders: ForceMovementOrdersType.CANCEL_ORDER);
+            ApiEventManager.OnMoveEnd.AddListener(this, _fiora, OnMoveEnd, true);
+            ApiEventManager.OnMoveSuccess.AddListener(this, _fiora, OnMoveSuccess, true);
+            
+            PlayAnimation(_fiora, "Spell1");
+            AddParticleTarget(_fiora, _fiora, "Fiora_Dance_windup.troy", _fiora);
+            AddParticleTarget(_fiora, _fiora, "FioraQLunge_dashtrail.troy", _fiora);
+            ForceMove(_fiora, GetMovePositionByCollisionOffset(_fiora, _target,0), 2200, 0f, ForceMovementType.FURTHEST_WITHIN_RANGE, ForceMovementOrdersFacing.FACE_MOVEMENT_DIRECTION, true, true, ForceMovementOrdersType.CANCEL_ORDER, "FioraQLunge");
         }
         
         // AICI ESTE REPARAȚIA: Am adăugat parametrul ForceMovementParameters
-        public void OnMoveSuccess(AttackableUnit unit, ForceMovementParameters parameters)
+        private void OnMoveSuccess(AttackableUnit unit, ForceMovementParameters parameters)
         {
-            Fiora.SetForceMovementState(false);
-            Damage = 15 + (25f * Fiora.Spells[0].CastInfo.SpellLevel) + (Fiora.Stats.AttackDamage.FlatBonus * 1.2f);
-            Target.TakeDamage(Fiora, Damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
-            AddParticleTarget(Fiora, Target, "FioraQLunge_tar", Target);
-            if (Fiora.Team != Target.Team && Target is Champion)
+            parameters:
+            var ad = _fiora.Stats.AttackDamage.FlatBonus * _spell.SpellData.Coefficient;
+            _damage = 15 + (25f * _fiora.Spells[0].CastInfo.SpellLevel) + (_fiora.Stats.AttackDamage.FlatBonus * 1.2f);
+            _target.TakeDamage(_fiora, _damage, DamageType.DAMAGE_TYPE_PHYSICAL, DamageSource.DAMAGE_SOURCE_SPELL, false);
+            AddParticleTarget(_fiora, _target, "FioraQLunge_tar", _target);
+            if (_fiora.Team != _target.Team && _target is Champion)
             {
-                Fiora.SetTargetUnit(Target, true);
-                Fiora.UpdateMoveOrder(OrderType.AttackTo, true);
+                _fiora.SetTargetUnit(_target, true);
             }
         }
         
         // AICI ESTE REPARAȚIA: Am adăugat parametrul ForceMovementParameters
-        public void OnMoveEnd(AttackableUnit owner, ForceMovementParameters parameters)
+        private void OnMoveEnd(AttackableUnit owner, ForceMovementParameters parameters)
         {
-            Fiora.SetForceMovementState(false);
-            SetStatus(Fiora, StatusFlags.Ghosted, false);
-            StopAnimation(Fiora, "spell1", StopAnimationFlags.StopAll | StopAnimationFlags.Fade | StopAnimationFlags.IgnoreLock);
+            SetStatus(_fiora, StatusFlags.Ghosted, false);
+            StopAnimation(_fiora, "spell1", StopAnimationFlags.StopAll | StopAnimationFlags.Fade | StopAnimationFlags.IgnoreLock);
         }
     }
 }
